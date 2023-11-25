b19aa26cf3b89745dd218dbc78a8196cf7642b1b
MikeIrlbeck <mirlbeck22@gmail.com>
2023-11-25 10:59:53 -0700

    added revenue and profit data

--- Analyzer.py
+++ Analyzer.py
@@ -3,7 +3,7 @@ from datetime import date
 from tabulate import tabulate
 
 
-class Purchase():
+class Item():
     def __init__(self, date, item, amount):
         self.date = date
         self.item = item
@@ -18,18 +18,32 @@ class YearOfPurchase:
         def __init__(self, month):
             self.month = month
             self.purchases = []
+            self.revenue = []
 
-        def add(self, purchase):
-            self.purchases.append(purchase)
+        def addExpense(self, item):
+            self.purchases.append(item)
+
+        def addRevenue(self, item):
+            self.revenue.append(item)
 
         def getMonth(self):
             return self.month
 
-        def getTotal(self):
+        @staticmethod
+        def _getTotal(items: list):
             total = 0.0
-            for p in self.purchases:
+            for p in items:
                 total = total + p.amount
             return total
+
+        def getTotalExpenses(self):
+            return self._getTotal(self.purchases)
+
+        def getTotalRevenue(self):
+            return self._getTotal(self.revenue)
+
+        def getTotalProfit(self):
+            return self.getTotalRevenue() - self.getTotalExpenses()
 
     def __init__(self, year):
         self.year = year
@@ -37,27 +51,51 @@ class YearOfPurchase:
         for i in range(1, 13):
             self.monthPurchases.append(self.MonthOfPurchase(i))
 
-    def add(self, purchase: Purchase):
-        if (purchase.date.month >= 1 and purchase.date.month < 13):
-            self.monthPurchases[purchase.date.month - 1].add(purchase)
+    def _addItem(self, item: Item, callback: None):
+        if (item.date.month >= 1 and item.date.month < 13):
+            callback(self.monthPurchases[item.date.month - 1], item)
 
-    def getTotal(self) -> float:
+    def addExpense(self, item: Item):
+        self._addItem(item, lambda x, y: x.addExpense(y))
+
+    def addRevenue(self, item: Item):
+        self._addItem(item, lambda x, y: x.addRevenue(y))
+
+    def _getTotal(self, x: None) -> float:
         total = 0.0
         for month in self.monthPurchases:
-            total += month.getTotal()
+            total += x(month)
         return total
+
+    def getTotalExpenses(self) -> float:
+        return self._getTotal(lambda x: x.getTotalExpenses())
+
+    def getTotalRevenue(self) -> float:
+        return self._getTotal(lambda x: x.getTotalRevenue())
+
+    def getTotalProfit(self) -> float:
+        return self._getTotal(lambda x: x.getTotalProfit())
 
     def __str__(self) -> str:
         output = "\n%d Financial Report\n" % self.year
 
+        formatNumber = "{:.2f}"
         data = []
         for mp in self.monthPurchases:
-            if (mp.getTotal() > 0.0):
+            if (mp.getTotalExpenses() > 0.0):
+                profit = mp.getTotalProfit()
                 data.append(
-                    [str(mp.getMonth()), "{:.2f}".format(mp.getTotal())])
+                    [str(mp.getMonth()),
+                     formatNumber.format(mp.getTotalExpenses()),
+                     formatNumber.format(mp.getTotalRevenue()),
+                     formatNumber.format(profit) if profit > 0 else ("(%s)" % formatNumber).format(abs(profit))])
 
-        data.append(["Total", "%.2f" % self.getTotal()])
-        output += tabulate(data, headers=['Month', 'Expense ($)'])
+        data.append(["Total",
+                     formatNumber.format(self.getTotalExpenses()),
+                     formatNumber.format(self.getTotalRevenue()),
+                     formatNumber.format(self.getTotalProfit())])
+        output += tabulate(data,
+                           headers=['Month', 'Expense ($)', 'Income ($)', 'Profit ($)'])
         return output
 
 
@@ -82,7 +120,7 @@ def read_finance_data(file_str: str) -> list:
             re.search(r"[0-9]+", dateList[1])) else int(1)
         thisDate = date(int(year), int(dateList[0]), day)
 
-        return Purchase(thisDate, dataList.group(2), dataList.group(3))
+        return Item(thisDate, dataList.group(2), dataList.group(3))
 
     def filter(line):
         return (re.search(r"^\s$", line)) is None  # true if no whitepsace
@@ -103,10 +141,17 @@ def read_finance_data(file_str: str) -> list:
 purchases = read_finance_data(
     "/Users/michaelirlbeck/Documents/FinanceAnalyzer/ExpenseData.txt")
 
+revenue = read_finance_data(
+    "/Users/michaelirlbeck/Documents/FinanceAnalyzer/IncomeData.txt")
 
-for purchase in purchases:
-    index = int(purchase.date.year) - 2022
-    yearPurchases[index].add(purchase)
+
+groups = []
+groups.append({'items': purchases, 'callback': lambda x, y: x.addExpense(y)})
+groups.append({'items': revenue, 'callback': lambda x, y: x.addRevenue(y)})
+for items in groups:
+    for item in items['items']:
+        index = int(item.date.year) - 2022
+        items['callback'](yearPurchases[index], item)
 
 for yp in yearPurchases:
     print(yp)
--- README.md
+++ README.md
@@ -8,28 +8,30 @@ To run the script:
 ## Recording expenses and income
 
 Place the following two files in the same directory as this repository:
+
 - ExpenseData.txt
 - IncomeData.txt
 
 Each line represents an expense or source of income with the following format:
 
 Format:
-```mm/dd/yr, Name, amount```
+`mm/dd/yr, Name, amount`
 
 Example:
-```11/23/23, Charity, $50```
+`11/23/23, Charity, $50`
 
 ## Output
+
 ```
 2022 Financial Report
-Month      Expense ($)
--------  -------------
-6               282.5
-7               280.01
-8               275.91
-9               368.15
-10             1358.03
-11              383.47
-12             1313.23
-Total          4261.3
+Month      Expense ($)    Income ($)  Profit ($)
+-------  -------------  ------------  ------------
+6               282.5           0     (282.50)
+7               280.01          0     (280.01)
+8               275.91       1755.89  1479.98
+9               368.15        962.8   594.65
+10             1358.03       1462.8   104.77
+11              383.47       1537.8   1154.33
+12             1313.23       2169.2   855.97
+Total          4261.3        7888.49  3627.19
 ```
