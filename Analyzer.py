ae71405a33ac335e593b88b74473383179d30a0d
MikeIrlbeck <mirlbeck22@gmail.com>
2023-11-25 09:50:07 -0700

    removed categorizes and cleaned up file reading

--- Analyzer.py
+++ Analyzer.py
@@ -1,11 +1,6 @@
 
-import json
 import re
-import os.path
 from datetime import date
-
-data = open(
-    "/Users/michaelirlbeck/Documents/FinanceAnalyzer/ExpenseData.txt", "r")
 
 
 class Purchase():
@@ -40,23 +35,21 @@ def parsePurchaseLine(line):
 
 purchases = []
 
-for x in data:
-    if filter(x):
-        try:
-            purchases.append(parsePurchaseLine(x))
-        except ValueError:
-            raise Exception("Error: " + str(x))
+with open("/Users/michaelirlbeck/Documents/FinanceAnalyzer/ExpenseData.txt", "r") as data:
 
-        # print("{} : {}".format(len(x), x))
+    for x in data:
+        if filter(x):
+            try:
+                purchases.append(parsePurchaseLine(x))
+            except ValueError:
+                raise Exception("Error: " + str(x))
 
-data.close()
 
 purchases.sort(key=lambda el: el.date)  # sort by date
 
 total = 0.0
 for purchase in purchases:
     total = total + float(purchase.amount)
-    # print(p)
 print("total amount: ${:.2f}".format(total))
 
 
@@ -114,104 +107,6 @@ print(len(yearPurchases))
 for purchase in purchases:
     index = int(purchase.date.year) - 2022
     yearPurchases[index].add(purchase)
-# ab = filter(lambda mp: mp.getTotal() > 0.0, monthPurchases)
-# TypeError: filter() takes 1 positional argument but 2 were given
-
-# filtered = []
-# for mp in monthPurchases:
-#     if (mp.getTotal() > 0.0):
-#         filtered.append(mp)
-
-# monthPurchases = filtered
 
 for yp in yearPurchases:
     print(yp)
-
-
-# Attempt to sort into categories
-class Category(object):
-    def __init__(self, name):  # , purchases = []): doing this causes shared data; why??
-        self.name = name
-        self.purchases = []
-
-    def add(self, purchase):
-        self.purchases.append(purchase)
-
-    def toJSON(self):
-        string = ""
-        append = ""
-        for p in self.purchases:
-            string = string + append + str(p)
-            append = ","
-        return {"name": self.name, "purchases": string}
-
-
-names = ["Car", "Groceries", "Eating Out", "Personal", "Miscellaneous"]
-
-categories = []
-
-for name in names:
-    categories.append(Category(name))
-
-string = ""
-m = 0
-for name in names:
-    string = "{}\t{}:{}".format(string, str(m), name)
-    m = m + 1
-
-print(string)
-
-m = 0
-for purchase in purchases:
-    i = input("{}: ".format(str(purchase.item)))
-    print("selected {}, len: {}".format(
-        categories[int(i)].name, len(categories[int(i)].purchases)))
-    categories[int(i)].add(purchase)
-    m = m + 1
-    if m > 3:
-        break
-
-# if os.path.exists(modelPath):
-#     print("Exists! - " + modelPath)
-#     mode = "r"
-# else:
-#     print("Does not exists - " + modelPath)
-#     mode = "w"
-
-modelPath = "/Users/michaelirlbeck/Desktop/Finances/Model.json"
-model = open(modelPath, "w+")
-
-for category in categories:
-    # model.write(str(category))
-    model.write(json.dumps(category.toJSON()) + "\n")
-
-model.close()
-
-model = open(modelPath, "r")
-
-loadedPurchases = []
-
-for line in model:
-    x = json.loads(line)
-    # print(str(type(x)))
-    # print(x)
-    # print("{}".format(x["name"]))
-    # print("{}".format(x["purchases"]))
-    # print("{}".format(x["purchases"].split(",")))
-
-    if x["purchases"]:
-        for purchase in x["purchases"].split(","):
-            data = purchase.split(":")
-            dateData = data[0].strip().split("-")
-            # create list of purchases
-            loadedPurchases.append(Purchase(date(int(dateData[0]), int(dateData[1]), int(dateData[2])),
-                                            data[1].strip(), data[2].strip()))
-            # recreate list of categories with their purchases
-
-total = 0.0
-for purchase in loadedPurchases:
-    total = total + float(purchase.amount)
-    # print(p)
-print("total amount: ${:.2f}".format(total))
-
-model.close()
